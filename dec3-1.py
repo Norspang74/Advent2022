d93997ab295a06664c441618b75bdc92ed214cac
Norspang74 <119138016+Norspang74@users.noreply.github.com>
2022-12-03 10:59:28 +0200

    Dag 3 part 1, using ASCII values for letter value

---
+++ dec3-1.py
@@ -0,0 +1,29 @@
+rucksack_first_items = []
+rucksack_second_items = []
+mistake_item =[]
+sum=0
+with open('rucksack.txt') as f:
+    line_number = 0
+    for line in f.readlines():
+        num_of_items = int((len(line) - (len(line)%2)))
+        num_of_items_in_compartment = int(num_of_items / 2) 
+        for item in range(0, num_of_items_in_compartment):
+            rucksack_first_items.append(line[item])
+        
+        for item in range(num_of_items_in_compartment, num_of_items):
+            rucksack_second_items.append(line[item])
+        
+        for item in rucksack_first_items:
+            if item in rucksack_second_items:
+                mistake_item.append(item)
+                add = ord(item)
+                if add > 95:
+                    sum += (add-96)
+                else:
+                    sum += (add-38)
+                break
+        
+        rucksack_first_items = []
+        rucksack_second_items = []
+        
+        print(sum)
